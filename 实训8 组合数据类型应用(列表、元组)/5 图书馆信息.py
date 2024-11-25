f88fc72d084e4359b0991d77f2d230df6c3c38d6
ht丶 <HuoTianBss@163.com>
2024-11-25 13:34:28 +0800

    ht

--- 实训8 组合数据类型应用(列表、元组)/5 图书馆信息.py
+++ 实训8 组合数据类型应用(列表、元组)/5 图书馆信息.py
@@ -1,11 +1,17 @@
-data='library.txt'
-buy_list={'item':[],'total_price':0}
-def load_data():
-    library=[]
-    with open(data,'r',encoding='utf-8') as f:
-        for line in f:
-            num,name,price,stock=line.strip().split('-')
-            library.append({'num':num,'name':name,'price':price,'stock':stock})
-        return library
-load_data()
-def eadit_data():
+library_list={'item':[],'total_price':0}
+def start():
+    num,name,price,stock=input('请输入图书的书号,书名,单价,数量(以逗号分隔):').split(',')
+    library_list['item'].append({'num': num, 'name': name, 'price': float(price), 'stock': int(stock)})
+    tf = input('是否继续输入图书(Y/N):').upper()
+    if tf != 'Y' and tf != 'N':
+        tf = input('是否继续输入图书(Y/N):').upper()
+    elif tf == 'Y':
+        start()
+    else:
+        for item in library_list['item']:
+            print(f'书号:{item['num']}-书名:{item['name']}-单价:{item['price']},数量:{item['stock']}')
+            library_list['total_price'] += item['price'] * item['stock']
+        print(f'总价:{library_list['total_price']}')
+
+if __name__ == '__main__':
+    start()
