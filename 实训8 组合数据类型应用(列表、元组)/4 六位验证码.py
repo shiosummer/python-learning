ddd361e769e2ae015ce3506eb0c947851bb24e01
ht丶 <2687565557@qq.com>
2024-11-28 18:13:19 +0800

    ht

--- 实训8 组合数据类型应用(列表、元组)/4 六位验证码.py
+++ 实训8 组合数据类型应用(列表、元组)/4 六位验证码.py
@@ -1,9 +1,15 @@
 import random
 chars_list=[]
 for i in range(6):
-    chars_list.append(random.randint(0,9))
+    tpye=random.randint(0,2)
+    if tpye == 0:
+        chars_list.append(random.randrange(0,9))
+    elif tpye == 1:
+        chars_list.append(chr(random.randrange(65,90)))
+    else:
+        chars_list.append(chr(random.randrange(97,122)))
 
-num=''
+str1 = ''
 for el in chars_list:
-    num += str(el)
-print(num)
+    str1 += str(el)
+print(str1)
--- 实训8 组合数据类型应用(列表、元组)/6 列表操作.py
+++ 实训8 组合数据类型应用(列表、元组)/6 列表操作.py
@@ -2,8 +2,7 @@ demo1_list=[1,2,'Python',[3,4,'d',0],5]
 print(f'列表:>>>{demo1_list}')
 demo1_list.append('Java')
 print(f"追加元素'Java':>>>{demo1_list}")
-place = demo1_list.index([3,4,'d',0])
-demo1_list[place]= 34
+demo1_list[demo1_list.index([3,4,'d',0])]= 34
 print(f"查找[3,4,'d',0],并将其改为34:>>>{demo1_list}")
 demo2_list=demo1_list[::-1]
 print(f"将列表进行逆置:>>>{demo2_list}")
