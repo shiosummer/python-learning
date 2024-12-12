7f5f80f79e9affbcef763ada82494b3f09d27cf1
ht丶 <HuoTianBss@163.com>
2024-12-12 19:28:47 +0800

    2024.12.12

--- 实训7 文件操作/3 hnjm.txt.py
+++ 实训7 文件操作/3 hnjm.txt.py
@@ -1,6 +1,9 @@
 with open('hnjm.txt','w',encoding='utf-8') as f:
     f.write('你好，欢迎来到河南经贸职业学院')
-with open ('hnjm.txt','r+',encoding='utf-8') as f:
-    s=f.read()
-    f.seek(s.find('河南经贸'))
-    print(f.tell())
+with open('hnjm.txt', 'r+', encoding='utf-8') as f:
+    s1 = f.read()
+    index = s1.find('河南经贸')
+    s2 = s1[:index] + '我爱' + s1[index:]
+    s2 = s2.replace('职业', '')
+    f.seek(0)
+    f.write(s2)
